0ff2ff7664642cb6b64ca055999d699ab04e36c1
Ramilbey <ramilbey505711503@gmail.com>
2025-03-31 05:09:26 +0800

    added blocks of chess

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function createNumbers() {
         let num = document.createElement("div");
         num.classList.add("chess-number", "left-numbers");
         num.textContent = 8 - i;
-        num.style.top = `${(i * 500) / 8 + 65}px`; // Adjusted for proper placement
+        num.style.top = `${(i * 520) / 8 + 65}px`; // Adjusted for proper placement
         container.appendChild(num);
     }
 
@@ -15,7 +15,7 @@ function createNumbers() {
         let letter = document.createElement("div");
         letter.classList.add("chess-number", "bottom-letters");
         letter.textContent = String.fromCharCode(65 + i); // A to H
-        letter.style.left = `${(i * 500) / 8 + 70}px`; // Adjusted for proper placement
+        letter.style.left = `${(i * 520) / 8 + 62}px`; // Adjusted for proper placement
         container.appendChild(letter);
     }
 }
@@ -26,5 +26,23 @@ createNumbers();
 function createBlock() {
     let blockCont = document.querySelector(".js-inside-cont")
 
-    for(let i = 0)
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            let square = document.createElement("div")
+            square.classList.add('chess-square')
+
+            if ((i + j) % 2 === 0 ) {
+                square.style.backgroundColor = "white"
+            } else {
+                square.style.backgroundColor = "black"
+            }
+            square.style.width = "12.5%"
+            square.style.height = "12.5%"
+            square.style.float = "left"
+            
+            blockCont.appendChild(square)
+
+        }
+    }
 }
+createBlock()
