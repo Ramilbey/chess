c8749f6ab63fa16b81777a0356c3d10098895599
Ramilbey <ramilbey505711503@gmail.com>
2025-04-02 21:52:12 +0800

    get html to js

--- details/piece.js
+++ details/piece.js
@@ -1,8 +1,8 @@
-const piece = [
-  {
-    name: "bishop",
-    id: "a1",
-    image:
-      "./pics/black-chess-bishop-piece-isolated-on-transparent-background-free-png.webp",
-  },
-];
+export const piece = [
+    {
+      name: "bishop",
+      id: "a1",
+      image: "./pics/black-chess-bishop-piece-isolated-on-transparent-background-free-png.webp",
+    },
+  ];
+  
