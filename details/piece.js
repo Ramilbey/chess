cb5f31b4333715840c597d797f4068e134c52f8f
Ramilbey <ramilbey505711503@gmail.com>
2025-04-02 21:32:05 +0800

    store data

---
+++ details/piece.js
@@ -0,0 +1,8 @@
+const piece = [
+  {
+    name: "bishop",
+    id: "a1",
+    image:
+      "./pics/black-chess-bishop-piece-isolated-on-transparent-background-free-png.webp",
+  },
+];
--- script.js
+++ script.js
@@ -48,3 +48,4 @@ function createBlock() {
 createBlock()
 
 
+function 
