fa6c18423b833349281726f5ad4b627ea463850f
Ramilbey <ramilbey505711503@gmail.com>
2025-04-05 22:41:35 +0800

    create a special affect on piece

---
+++ details/effect.js
@@ -0,0 +1,20 @@
+import {pieceHtml} from "./positon.js"
+
+document.querySelector('.js-blocks').innerHTML = pieceHtml;
+
+const nameDisplay = document.createElement('div')
+nameDisplay.classList.add('.selected-name')
+document.body.appendChild(nameDisplay);
+document.querySelectorAll('.image-piece').forEach((img) => {
+    img.addEventListener('click', () => {
+        img.classList.toggle('selected');
+      
+        const name = img.previousElementSibling?.textContent;
+
+        if (img.classList.contains('selected')) {
+            nameDisplay.textContent = `Selected: ${name}`;
+        } else {
+            nameDisplay.textContent = '';
+        }
+    });
+});
