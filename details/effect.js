752875b707f49a6f1550a28283ed02b95d4be338
Ramilbey <ramilbey505711503@gmail.com>
2025-04-06 22:53:07 +0800

    changed the js code

--- details/effect.js
+++ details/effect.js
@@ -5,16 +5,14 @@ document.querySelector('.js-blocks').innerHTML = pieceHtml;
 const nameDisplay = document.createElement('div')
 nameDisplay.classList.add('.selected-name')
 document.body.appendChild(nameDisplay);
+document.querySelector('.js-blocks').innerHTML = pieceHtml;
+
 document.querySelectorAll('.image-piece').forEach((img) => {
     img.addEventListener('click', () => {
-        img.classList.toggle('selected');
-      
-        const name = img.previousElementSibling?.textContent;
+        const wrapper = img.parentElement; // .pawn-block
+        const nameTag = wrapper.querySelector('.js-text');
 
-        if (img.classList.contains('selected')) {
-            nameDisplay.textContent =  name;
-        } else {
-            nameDisplay.textContent = '';
-        }
+        img.classList.toggle('selected');
+        nameTag.classList.toggle('visible-name');
     });
 });
